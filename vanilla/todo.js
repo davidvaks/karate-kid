3b6d80bb68f456d02e7ae9a57b3e0f786386fb72
davidv <davidv@wix.com>
2022-03-03 08:16:49 +0200

    refactor js file

--- vanilla/todo.js
+++ vanilla/todo.js
@@ -4,12 +4,13 @@ const ButtonTexts = {
     SAVE: 'Save'
 };
 
-const SingleTaskParts = {
+const SingleTaskClassNames = {
     CHECKBOX: 'task_checkbox',
     TEXT: 'task_text',
     EDIT_INPUT: 'task_edit_input',
     EDIT_BUTTON: 'task_edit_button',
-    REMOVE_BUTTON: 'task_remove_button'
+    REMOVE_BUTTON: 'task_remove_button',
+    STRIKE_THROUGH: 'task_strike_through',
 }
 
 const addTask = () => {
@@ -24,51 +25,35 @@ const addTask = () => {
 };
 
 const createTaskNewRow = (table, taskInput) => {
-    const checkbox = document.createElement('input');
+    const checkbox = createCheckbox();
     const taskText = createTaskTextElement(taskInput);
-    const editButton = document.createElement('input');
-    const deleteButton = document.createElement('input');
-
-    checkbox.type = 'checkbox';
-    checkbox.className = SingleTaskParts.CHECKBOX;
-
-    editButton.type = 'button';
-    editButton.className = SingleTaskParts.EDIT_BUTTON;
-    editButton.value = ButtonTexts.EDIT;
-
-    deleteButton.type = 'button';
-    deleteButton.className = SingleTaskParts.REMOVE_BUTTON;
-    deleteButton.value = ButtonTexts.REMOVE;
+    const editButton = createEditButton();
+    const deleteButton = createRemoveButton();
 
     const row = table.insertRow();
     row.insertCell(0).appendChild(checkbox);
     row.insertCell(1).appendChild(taskText);
     row.insertCell(2).appendChild(editButton);
     row.insertCell(3).appendChild(deleteButton);
-
-    deleteButton.addEventListener("click", (event) => deleteItem(event), false);
-    editButton.addEventListener("click", (event) => editItem(event), false);
-    checkbox.addEventListener("change", (event) => itemCheckboxChange(event), false);
 };
 
 const itemCheckboxChange = (event) => {
     const row = event.path[2];
     const taskTextCell = row.cells[1];
-    const taskTextElement = taskTextCell.querySelector('p');
+    const taskTextElement = taskTextCell.querySelector(selectorFor(SingleTaskClassNames.TEXT));
     
     if (event.target.checked) {
-        const strikeThrough = document.createElement('del');
+        const strikeThrough = createStrikeThroughElement();
         taskTextCell.appendChild(strikeThrough);
         strikeThrough.appendChild(taskTextElement);
     } else {
-        const text = taskTextCell.querySelector('p');
-        const strikeThrough = taskTextCell.querySelector('del');
-        taskTextCell.appendChild(text);
+        const strikeThrough = taskTextCell.querySelector(selectorFor(SingleTaskClassNames.STRIKE_THROUGH));
+        taskTextCell.appendChild(taskTextElement);
         strikeThrough.remove();
     };
 };
 
-const deleteItem = (event) => {
+const removeItem = (event) => {
     const row = event.path[2];
     row.remove();
 };
@@ -79,7 +64,7 @@ const editItem = (event) => {
     const taskTextCell = row.cells[1];
     const checkbox = row.cells[0].firstChild;
 
-    if (itemEditMode(editButton)) {
+    if (isItemEditMode(editButton)) {
         exitEditModeAndUpdate(checkbox, taskTextCell, editButton);
     } else {
         enterItemEditMode(checkbox, taskTextCell, editButton);
@@ -87,12 +72,9 @@ const editItem = (event) => {
 };
 
 const enterItemEditMode = (checkbox, taskTextCell, editButton) => {
-    const taskTextElement = taskTextCell.querySelector('p');
+    const taskTextElement = taskTextCell.querySelector(selectorFor(SingleTaskClassNames.TEXT));
 
-    const editInput = document.createElement('input');
-    editInput.type = 'text';
-    editInput.className = SingleTaskParts.EDIT_INPUT;
-    editInput.value = taskTextElement.textContent;
+    const editInput = createTaskEditInput(taskTextElement.textContent);
 
     taskTextCell.appendChild(editInput);
     taskTextElement.remove();
@@ -102,7 +84,7 @@ const enterItemEditMode = (checkbox, taskTextCell, editButton) => {
 };
 
 const exitEditModeAndUpdate = (checkbox, taskTextCell, editButton) => {
-    const newTaskInput = taskTextCell.querySelector('input');
+    const newTaskInput = taskTextCell.querySelector(selectorFor(SingleTaskClassNames.EDIT_INPUT));
     const newTaskText = createTaskTextElement(newTaskInput.value);
     taskTextCell.appendChild(newTaskText);
     newTaskInput.remove();
@@ -110,17 +92,61 @@ const exitEditModeAndUpdate = (checkbox, taskTextCell, editButton) => {
     checkbox.disabled = false;
 };
 
+const toggleEditButtonText = (editButton) => {
+    isItemEditMode(editButton) ? editButton.value = ButtonTexts.EDIT : editButton.value = ButtonTexts.SAVE;
+};
+
+const isItemEditMode = (editButton) => {
+    return editButton.value == ButtonTexts.SAVE;
+};
+
+const selectorFor = (className) => '.' + className;
+
+//     Elements Creation
+
 const createTaskTextElement = (taskInput) => {
     const taskText = document.createElement('p');
     taskText.textContent = taskInput;
-    taskText.className = SingleTaskParts.TEXT;
+    taskText.className = SingleTaskClassNames.TEXT;
     return taskText;
-}
-
-const toggleEditButtonText = (editButton) => {
-    itemEditMode(editButton) ? editButton.value = ButtonTexts.EDIT : editButton.value = ButtonTexts.SAVE;
 };
 
-const itemEditMode = (editButton) => {
-    return editButton.value == ButtonTexts.SAVE;
+const createTaskEditInput = (taskText) => {
+    const editInput = document.createElement('input');
+    editInput.type = 'text';
+    editInput.className = SingleTaskClassNames.EDIT_INPUT;
+    editInput.value = taskText;
+    return editInput;
+};
+
+const createStrikeThroughElement = () => {
+    const strikeThrough = document.createElement('del');
+    strikeThrough.className = SingleTaskClassNames.STRIKE_THROUGH;
+    return strikeThrough;
+};
+
+const createCheckbox = () => {
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.className = SingleTaskClassNames.CHECKBOX;
+    checkbox.addEventListener("change", (event) => itemCheckboxChange(event), false);
+    return checkbox;
+};
+
+const createEditButton = () => {
+    const editButton = document.createElement('input');
+    editButton.type = 'button';
+    editButton.className = SingleTaskClassNames.EDIT_BUTTON;
+    editButton.value = ButtonTexts.EDIT;
+    editButton.addEventListener("click", (event) => editItem(event), false);
+    return editButton;
+};
+
+const createRemoveButton = () => {
+    const removebutton = document.createElement('input');
+    removebutton.type = 'button';
+    removebutton.className = SingleTaskClassNames.REMOVE_BUTTON;
+    removebutton.value = ButtonTexts.REMOVE;
+    removebutton.addEventListener("click", (event) => removeItem(event), false);
+    return removebutton;
 };
